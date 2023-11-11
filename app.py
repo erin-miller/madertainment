5d8a969e18977436b62b4a5bb347e44a8eb65e5e
erin.miller <erinm1018@gmail.com>
2023-11-11 14:53:16 -0600

    add: react app

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/api/data')
+def get_data():
+    # Replace this with your actual data retrieval logic
+    data = [
+        {'id': 1, 'name': 'Item 1'},
+        {'id': 2, 'name': 'Item 2'},
+        # Add more data as needed
+    ]
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
