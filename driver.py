a53f0c8617955ddd38c2addad9510b69446b4d1b
caydenwelter <caydenwelter@gmail.com>
2023-11-11 14:18:35 -0600

    Create driver.py

---
+++ driver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+def get_driver():
+    service = Service(executable_path="chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')  # Run in headless mode
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    return driver
