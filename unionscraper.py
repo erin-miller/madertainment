d3338e8dcd5752889273ed52ebf347ba48946a75
caydenwelter <caydenwelter@gmail.com>
2023-11-11 18:48:33 -0600

    Update unionscraper.py
    Known working unionscraper.py. should be complete

--- unionscraper.py
+++ unionscraper.py
@@ -2,9 +2,28 @@ import driver
 import pandas as pd
 import time
 from selenium.common.exceptions import NoSuchElementException
+import eventdate, eventtime, eventlocation, eventsetting
+from datetime import datetime
+import re
 
 driver = driver.get_driver()
 driver.get("https://union.wisc.edu/events-and-activities/event-calendar/")
+
+# dict for converting Union-specific month format "JAN, FEB, etc." to month numbers 01-12
+month_dict = {
+    'JAN': '01',
+    'FEB': '02',
+    'MAR': '03',
+    'APR': '04',
+    'MAY': '05',
+    'JUN': '06',
+    'JUL': '07',
+    'AUG': '08',
+    'SEP': '09',
+    'OCT': '10',
+    'NOV': '11',
+    'DEC': '12'
+}
 
 def load_events(seconds = 3):
     # locate the "load more" button
@@ -43,23 +62,23 @@ def get_events():
 
         # get month
         month = format_data(raw_event, "class name", "event-list-item--date-badge--month")
-        event["month"] = month
+        # event["month"] = month
 
         # get day
         day = format_data(raw_event, "class name", "event-list-item--date-badge--day")
-        event["day"] = day
+        # event["day"] = day
 
         # get name
         name = format_data(raw_event, "tag name", "h3")
         event["name"] = name
 
         # get category
-        category = format_data(raw_event, "class name", "event-list-item-category")
+        category = format_data(raw_event, "class name", "event-list-item--category")
         event["category"] = category
 
         # get location
         location = format_data(raw_event, "class name", "event-list-item--location")
-        event["location"] = location
+        # event["location"] = location
 
         # get price
         price = format_data(raw_event, "class name", "event-list-item--tickets")
@@ -67,24 +86,43 @@ def get_events():
 
         # get time
         time = format_data(raw_event, "class name", "event-list-item--time")
-        event["time"] = time
+        # event["time"] = time
 
         # get description
         description = format_data(raw_event, "class name", "event-list-item--desc")
         event["description"] = description
+        
+        # convert the month 'APR' into '04'
+        month_as_num = month_dict[month]
+        
+        # standardize the date format. Union's website formats the 3rd as '3'. convert it to '03'.
+        day_extended = day
+        if len(day) == 1:
+            day_extended = "0" + day
 
-        # setting - this is the key for the event
-        # TODO: calculate setting
-            # TODO: create date format
-            # TODO: calculate date, time, location and convert to setting
+        # get the current year. start by assuming the event is in this year
+        event_year = datetime.now().year
 
-        # TODO: events[setting] = event
+        # if the event's month is less than the current month, we know it's in the next calendar year
+        if int(month_as_num) < datetime.now().month:
+            event_year += 1
 
-        # convert the event dict into a pandas series and append it to the events list
-        events.append(pd.Series(event))
+        event_date = eventdate.EventDate(month_as_num + '-' + day_extended + '-' + str(event_year))
+
+        # remove spaces from event time to standardize
+        event_time = re.sub(r"\s", "", time)
+
+        # create event_setting
+        event_setting = eventsetting.EventSetting(event_date, event_time, location)
+
+        # add event_setting to the event so we can access setting information later
+        event["setting"] = event_setting
+
+        # append the event to events as a pandas series using the setting as the key
+        events[event_setting] = pd.Series(event)
     
-    # TODO: convert events into a pandas series
-    return events
+    # convert events into a pandas series and return
+    return pd.Series(events)
 
 def format_data(event, tag_or_class, tag_or_class_name):
     # try to grab the data, and return None if for some reason the tag/class isn't found
@@ -99,4 +137,4 @@ def format_data(event, tag_or_class, tag_or_class_name):
 # load_events()
 # events = get_events()
 # for e in events:
-#     print(e["description"])
+#     print(e.to_json())
