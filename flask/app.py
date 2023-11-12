c70af47c228fe8755f69c6e395d373821fe1213a
erin.miller <erinm1018@gmail.com>
2023-11-12 00:45:49 -0600

    add: send json file to react

--- flask/app.py
+++ flask/app.py
@@ -1,13 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-@app.route('/api/data', methods=['GET'])
+@app.route('/flask/get_data')
 def get_data():
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
-
+    json_file_path = os.path.join(app.root_path, 'data.json', 'data.json')
+    return send_file(json_file_path, mimetype='application/json')
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
