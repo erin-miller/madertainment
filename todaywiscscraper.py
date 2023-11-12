41ba679d350cccd09cee9069d60e5761b1e168bc
caydenwelter <caydenwelter@gmail.com>
2023-11-11 23:36:07 -0600

    Update todaywiscscraper.py
    for some reason https://today.wisc.edu/events/index.html?page=1 skips today, so added a manual scrape for https://today.wisc.edu/events/day/<today>

--- todaywiscscraper.py
+++ todaywiscscraper.py
@@ -155,6 +155,75 @@ def get_events():
         index += 1
         driver1.get("https://today.wisc.edu/events/index.html?page=" + str(index))
 
+
+    # after the while loop, go back to https://today.wisc.edu/events/day/<TODAY> in "YYYY-MM-DD" format and scrape that
+    # for some reason https://today.wisc.edu/events/index.html?page=1 skips the current day
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    url = "https://today.wisc.edu/events/day/" + current_date
+    driver1.get(url)
+    
+    headers = driver1.find_elements("class name", "day-row-header")
+
+    # get the events lists
+    events_lists_by_day = driver1.find_elements("class name", "events-list")
+
+    # iterate through each day. events_list_by_day[i] cooresponds to the events on day headers[i]
+    for i in range(len(headers)):
+        # get the raw day in format: Sunday, November 12, 2023
+        day_raw = headers[i].text
+
+        # process day_raw into "MM-DD-YYYY" format
+        year = day_raw.split(", ")[2]
+        month = month_to_number[day_raw.split(", ")[1].split(" ")[0]]
+        day = day_raw.split(", ")[1].split(" ")[1]
+        day_extended = day
+        if len(day) == 1:
+            day_extended = "0" + day
+        date = month + "-" + day_extended + "-" + year
+
+        # get a list of events in this day
+        events_in_day = events_lists_by_day[i].find_elements("class name", "event-details")
+
+        # loop through each event in the day
+        for j in range(len(events_in_day)):
+            # create event dict to be added to events later
+            event = {}
+
+            # get the link on the name of the event to check in the next condition
+            link = events_in_day[j].find_element("tag name", "a").get_property("href")
+
+            pattern = re.compile(r'today\.wisc\.edu')
+            match = pattern.search(link)
+            if match:
+                links[link] = date # add the link to be multithreaded later
+            else:
+                # extract data
+                name = format_data(events_in_day[j], "class name", "event-title")
+                category = None # TODO: maybe use keywords to generate a category?
+                price = None # no price information on this page :(
+                description = None # no real description either
+                event_date = eventdate.EventDate(date)
+                time = format_data(events_in_day[j], "class name", "event-time")
+                if time is not None:
+                    time = re.sub(r"\s", "", time)
+                event_location = format_data(events_in_day[j], "class name", "event-location")
+
+                setting = eventsetting.EventSetting(event_date, time, event_location)
+
+                # load the event into event
+                event["name"] = name
+                event["category"] = category
+                event["price"] = price
+                event["description"] = description
+                event["setting"] = setting
+
+                event = pd.Series(event)
+
+                # add event to events using setting as key and event as value
+                events[setting] = event
+
     # call scrape() to scrape the https://today.wisc.edu/events/view/<n> urls
     scrape()
 
@@ -172,15 +241,15 @@ def format_data(event, tag_or_class, tag_or_class_name):
     return data
 
 # debug
-# get_events()
-# print()
-# print(len(links))
-# print(len(events))
+get_events()
+print()
+print(len(links))
+print(len(events))
 
-# events_pd = pd.Series(events)
+events_pd = pd.Series(events)
 
-# json_data = events_pd.to_json()
+json_data = events_pd.to_json()
 
-# # Save the JSON data to a local file
-# with open('output.json', 'w') as json_file:
-#     json_file.write(json_data)
+# Save the JSON data to a local file
+with open('output.json', 'w') as json_file:
+    json_file.write(json_data)
