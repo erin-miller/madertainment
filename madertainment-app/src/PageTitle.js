eddf7b8bc0c04af6b9d962297c556a44720b1a51
erin.miller <erinm1018@gmail.com>
2023-11-11 20:36:02 -0600

    add: page title

---
+++ madertainment-app/src/PageTitle.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const PageTitle = ({ title }) => {
+    return (
+        <div className="page-title">
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+export default PageTitle;
