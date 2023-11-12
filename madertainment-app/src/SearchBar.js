4821e1d6b1ceadf5f6283e4f9c6c94128ac9a148
erin.miller <erinm1018@gmail.com>
2023-11-12 05:42:30 -0600

    add: searchbar skeleton

---
+++ madertainment-app/src/SearchBar.js
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const handleSearch = () => {
+        console.log("Search term: ", searchTerm);
+    }
+
+    return (
+      <div>
+        <input type="text" placeholder="Search..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
+        <button onClick={handleSearch}>Search</button>
+      </div>
+    );
+  }
+
+  export default SearchBar;
